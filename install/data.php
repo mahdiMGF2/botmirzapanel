d01e50a07bbb167f102205b66f0bac54d4b4e7b4
Mahdi <mahdigholipour@outlook.com>
2023-12-19 16:41:04 +0330

    Update data.php

--- install/data.php
+++ install/data.php
@@ -39,7 +39,7 @@ $patternuser = '/\$usernamedb\s*=\s*".*?";/';
 $newFileContent = preg_replace($patternuser, '$usernamedb = "'.$form_data['dbuser'].'";', $newFileContent);
 // تغییر مقدار $dbdbpassword
 $patternpass = '/\$passworddb\s*=\s*".*?";/';
-$newFileContent = preg_replace($patternpass, '$passworddb = "'.$form_data['passworddb'].'";', $newFileContent);
+$newFileContent = preg_replace($patternpass, '$passworddb = "'.$form_data['dbpassword'].'";', $newFileContent);
 
 // تغییر مقدار $API_KEY
 $patterntoken = '/\$APIKEY\s*=\s*".*?";/';
@@ -50,7 +50,7 @@ $patternidadmin = '/\$adminnumber\s*=\s*".*?";/';
 $newFileContent = preg_replace($patternidadmin, '$adminnumber = "'.$form_data['idadmin'].'";', $newFileContent);
 
 try {
-    $connect = new mysqli('localhost', $form_data['dbuser'], $$form_data['dbpassword'], $$form_data['dbname']);
+    $connect = new mysqli('localhost', $form_data['dbuser'], $form_data['dbpassword'], $$form_data['dbname']);
     
     if ($connect->connect_errno) {
         $textdatabase = 'خطا در اتصال به پایگاه داده: ' . $connect->connect_error;
