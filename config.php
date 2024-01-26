00368fb6e590e097618d6b7c559b9b6dcfe69961
Mahdi <mahdigholipour@outlook.com>
2024-01-26 22:19:52 +0330

    Update config.php

--- config.php
+++ config.php
@@ -17,3 +17,18 @@ $APIKEY = "**TOKEN**"; // توکن ربات خود را وارد کنید
 $adminnumber = "5522424631";// آیدی عددی ادمین
 $domainhosts = "domain.com/bot";// دامنه  هاست و مسیر سورس
 $usernamebot = "marzbaninfobot"; //نام کاربری ربات  بدون @
+
+
+
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+$dsn = "mysql:host=localhost;dbname=$dbname;charset=utf8mb4";
+try {
+     $pdo = new PDO($dsn, $usernamedb, $passworddb, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
