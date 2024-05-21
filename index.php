4ddddb63e640ba766378891cc9e89587ddb09a54
mahdi <mahdigholipour@outlook.com>
2024-05-21 19:24:35 +0330

    update

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 ini_set('error_log', 'error_log');
-$version = "4.8.4";
+$version = "4.8.5";
 date_default_timezone_set('Asia/Tehran');
 require_once 'config.php';
 require_once 'botapi.php';
