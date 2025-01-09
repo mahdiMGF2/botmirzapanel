165b32dff74721ac429f2703506f225cc41f530f
Ilya <86963582+ILYAGVC@users.noreply.github.com>
2025-01-09 12:30:09 +0330

    Update functions.php

--- functions.php
+++ functions.php
@@ -427,9 +427,25 @@ function sanitizeUserName($userName) {
 }
 function checktelegramip(){
     global $secrettoken;
+
+    if (empty($secrettoken) {
+$telegram_ip_ranges = [
+        ['lower' => '149.154.160.0', 'upper' => '149.154.175.255'],
+        ['lower' => '91.108.4.0',    'upper' => '91.108.7.255']
+    ];
+    $ip_dec = (float) sprintf("%u", ip2long($_SERVER['REMOTE_ADDR']));
+    $ok = false;
+    foreach ($telegram_ip_ranges as $telegram_ip_range) if (!$ok) {
+        $lower_dec = (float) sprintf("%u", ip2long($telegram_ip_range['lower']));
+        $upper_dec = (float) sprintf("%u", ip2long($telegram_ip_range['upper']));
+        if ($ip_dec >= $lower_dec and $ip_dec <= $upper_dec) $ok = true;
+    }
+    return $ok;
+    } else {
     if ($_SERVER['HTTP_X_TELEGRAM_BOT_API_SECRET_TOKEN'] == $secrettoken) {
         return true;
     } else {
         return false;
     }
+ }
 }
