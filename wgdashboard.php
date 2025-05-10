8d6303d5ada899c1435a1758d29a3f7b71719a32
Mahdi <mahdigholipour@outlook.com>
2025-05-10 19:24:45 +0330

    Compatibility with the new version of the wgdashboard panel

--- wgdashboard.php
+++ wgdashboard.php
@@ -3,10 +3,11 @@ require_once 'config.php';
 ini_set('error_log', 'error_log');
 
 
-function loginwg($url,$username,$password){
+function loginwg($url, $username, $password)
+{
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $url.'/api/authenticate',
+        CURLOPT_URL => $url . '/api/authenticate',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -31,15 +32,16 @@ function loginwg($url,$username,$password){
         return $token;
     }
     curl_close($curl);
-    return json_decode($response,true);
+    return json_decode($response, true);
 }
-function get_userwg($username,$namepanel){
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(isset($loginpanel['errror']))return;
+function get_userwg($username, $namepanel)
+{
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (isset($loginpanel['errror'])) return;
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/getWireguardConfigurationInfo?configurationName='.$marzban_list_get['inboundid'],
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/getWireguardConfigurationInfo?configurationName=' . $marzban_list_get['inboundid'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -52,12 +54,12 @@ function get_userwg($username,$namepanel){
         ),
         CURLOPT_COOKIEFILE => 'cookiewg.txt',
     ));
-    $response = json_decode(curl_exec($curl),true);
-    if(!isset($response))return;
-    $outputpear = array_merge($response['data']['configurationPeers'],$response['data']['configurationRestrictedPeers']);
+    $response = json_decode(curl_exec($curl), true);
+    if (!isset($response)) return;
+    $outputpear = array_merge($response['data']['configurationPeers'], $response['data']['configurationRestrictedPeers']);
     $output = [];
-    foreach ($outputpear as $userinfo){
-        if($userinfo['name'] == $username){
+    foreach ($outputpear as $userinfo) {
+        if ($userinfo['name'] == $username) {
             $output = $userinfo;
             break;
         }
@@ -66,15 +68,16 @@ function get_userwg($username,$namepanel){
     unlink('cookiewg.txt');
     return $output;
 }
-function ipslast($namepanel){
+function ipslast($namepanel)
+{
 
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(isset($loginpanel['errror']))return;
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (isset($loginpanel['errror'])) return;
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/getAvailableIPs/'.$marzban_list_get['inboundid'],
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/getAvailableIPs/' . $marzban_list_get['inboundid'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -87,19 +90,21 @@ function ipslast($namepanel){
         ),
         CURLOPT_COOKIEFILE => 'cookiewg.txt',
     ));
-    $response = json_decode(curl_exec($curl),true)['data'];
+    $response = json_decode(curl_exec($curl), true)['data'];
+    $key = array_keys($response)[0];
     curl_close($curl);
-    return $response[0];
+    return $response[$key][0];
 }
-function downloadconfig($namepanel,$publickey){
+function downloadconfig($namepanel, $publickey)
+{
 
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(isset($loginpanel['errror']))return;
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (isset($loginpanel['errror'])) return;
     $curl = curl_init();
 
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel']."/api/downloadPeer/{$marzban_list_get['inboundid']}?id=".urlencode($publickey),
+        CURLOPT_URL => $marzban_list_get['url_panel'] . "/api/downloadPeer/{$marzban_list_get['inboundid']}?id=" . urlencode($publickey),
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -112,17 +117,18 @@ function downloadconfig($namepanel,$publickey){
         ),
         CURLOPT_COOKIEFILE => 'cookiewg.txt',
     ));
-    $response = json_decode(curl_exec($curl),true)['data'];
+    $response = json_decode(curl_exec($curl), true)['data'];
     curl_close($curl);
     unlink('cookiewg.txt');
     return $response;
 }
-function addpear($namepanel, $usernameac){
+function addpear($namepanel, $usernameac)
+{
 
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
     $pubandprivate = publickey();
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if($loginpanel['status'] == false)return;
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if ($loginpanel['status'] == false) return;
     $config = array(
         'name' => $usernameac,
         'allowed_ips' => [ipslast($namepanel)],
@@ -131,10 +137,10 @@ function addpear($namepanel, $usernameac){
         'preshared_key' => $pubandprivate['preshared_key'],
     );
 
-    $configpanel = json_encode($config,true);
+    $configpanel = json_encode($config, true);
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/addPeers/'.$marzban_list_get['inboundid'],
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/addPeers/' . $marzban_list_get['inboundid'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -150,18 +156,19 @@ function addpear($namepanel, $usernameac){
             'Content-Type: application/json',
         ),
     ));
-    $response =json_decode(curl_exec($curl),true);
+    $response = json_decode(curl_exec($curl), true);
     $config['status'] = true;
-    if($response['status'] == false)return $response;
+    if ($response['status'] == false) return $response;
 
     curl_close($curl);
     unlink('cookiewg.txt');
     return $config;
 }
-function setjob($namepanel,$type,$value,$publickey){
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(isset($loginpanel['errror']))return;
+function setjob($namepanel, $type, $value, $publickey)
+{
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (isset($loginpanel['errror'])) return;
     $curl = curl_init();
     $data = json_encode(array(
         "Job" => array(
@@ -174,9 +181,10 @@ function setjob($namepanel,$type,$value,$publickey){
             "CreationDate" => "",
             "ExpireDate" => null,
             "Action" => "restrict"
-        )));
+        )
+    ));
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/savePeerScheduleJob/',
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/savePeerScheduleJob',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -196,18 +204,18 @@ function setjob($namepanel,$type,$value,$publickey){
     return $response;
     unlink('cookiewg.txt');
     return $response;
-
 }
-function updatepear($namepanel,array $config){
+function updatepear($namepanel, array $config)
+{
 
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
-    $configpanel = json_encode($config,true);
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
+    $configpanel = json_encode($config, true);
 
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/updatePeerSettings/'.$marzban_list_get['inboundid'],
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/updatePeerSettings/' . $marzban_list_get['inboundid'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -229,16 +237,17 @@ function updatepear($namepanel,array $config){
     unlink('cookiewg.txt');
     return json_decode($response, true);
 }
-function deletejob($namepanel,array $config){
+function deletejob($namepanel, array $config)
+{
 
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
     $configpanel = json_encode($config);
 
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/deletePeerScheduleJob/',
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/deletePeerScheduleJob/',
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -259,20 +268,21 @@ function deletejob($namepanel,array $config){
     unlink('cookiewg.txt');
     return json_decode($response, true);
 }
-function ResetUserDataUsagewg($publickey, $namepanel){
+function ResetUserDataUsagewg($publickey, $namepanel)
+{
 
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $namepanel, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
     $config = array(
         "id" => $publickey,
         "type" => "total"
     );
-    $configpanel = json_encode($config,true);
+    $configpanel = json_encode($config, true);
 
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/resetPeerData/'.$marzban_list_get['inboundid'],
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/resetPeerData/' . $marzban_list_get['inboundid'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -294,17 +304,18 @@ function ResetUserDataUsagewg($publickey, $namepanel){
 }
 
 
-function remove_userwg($location,$username){
-    allowAccessPeers($location,$username);
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
-    $data_user = json_decode(select("invoice","user_info","username",$username,"select")['user_info'],true)['public_key'];
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
+function remove_userwg($location, $username)
+{
+    allowAccessPeers($location, $username);
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
+    $data_user = json_decode(select("invoice", "user_info", "username", $username, "select")['user_info'], true)['public_key'];
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/deletePeers/'.$marzban_list_get['inboundid'],
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/deletePeers/' . $marzban_list_get['inboundid'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -322,22 +333,23 @@ function remove_userwg($location,$username){
             'Content-Type: application/json',
         ),
     ));
-    $response = json_decode(curl_exec($curl),true);
+    $response = json_decode(curl_exec($curl), true);
     curl_close($curl);
     unlink('cookiewg.txt');
     return $response;
 }
-function allowAccessPeers($location,$username){
+function allowAccessPeers($location, $username)
+{
 
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
-    $data_user = json_decode(select("invoice","user_info","username",$username,"select")['user_info'],true)['public_key'];
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
+    $data_user = json_decode(select("invoice", "user_info", "username", $username, "select")['user_info'], true)['public_key'];
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/allowAccessPeers/'.$marzban_list_get['inboundid'],
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/allowAccessPeers/' . $marzban_list_get['inboundid'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -355,22 +367,23 @@ function allowAccessPeers($location,$username){
             'Content-Type: application/json',
         ),
     ));
-    $response = json_decode(curl_exec($curl),true);
+    $response = json_decode(curl_exec($curl), true);
     curl_close($curl);
     unlink('cookiewg.txt');
     return $response;
 }
-function restrictPeers($location,$username){
+function restrictPeers($location, $username)
+{
 
-    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location,"select");
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
-    $data_user = json_decode(select("invoice","user_info","username",$username,"select")['user_info'],true)['public_key'];
-    $loginpanel = loginwg($marzban_list_get['url_panel'],$marzban_list_get['username_panel'],$marzban_list_get['password_panel']);
-    if(!$loginpanel['status'])return;
+    $marzban_list_get = select("marzban_panel", "*", "name_panel", $location, "select");
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
+    $data_user = json_decode(select("invoice", "user_info", "username", $username, "select")['user_info'], true)['public_key'];
+    $loginpanel = loginwg($marzban_list_get['url_panel'], $marzban_list_get['username_panel'], $marzban_list_get['password_panel']);
+    if (!$loginpanel['status']) return;
     $curl = curl_init();
     curl_setopt_array($curl, array(
-        CURLOPT_URL => $marzban_list_get['url_panel'].'/api/restrictPeers/'.$marzban_list_get['inboundid'],
+        CURLOPT_URL => $marzban_list_get['url_panel'] . '/api/restrictPeers/' . $marzban_list_get['inboundid'],
         CURLOPT_RETURNTRANSFER => true,
         CURLOPT_ENCODING => '',
         CURLOPT_MAXREDIRS => 10,
@@ -388,7 +401,7 @@ function restrictPeers($location,$username){
             'Content-Type: application/json',
         ),
     ));
-    $response = json_decode(curl_exec($curl),true);
+    $response = json_decode(curl_exec($curl), true);
     curl_close($curl);
     unlink('cookiewg.txt');
     return $response;
