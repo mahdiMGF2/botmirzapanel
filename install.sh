a5db5d7583d940d707513a47006691aa6eb6f46e
mahdiis <135647913+mahdiismailpuri@users.noreply.github.com>
2025-06-03 12:48:01 +0330

    Version 0.14

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ function show_logo() {
     echo "================================================================================="
     echo -e "\033[0m"
     echo ""
-    echo -e "\033[1;36mVersion:\033[0m \033[33m0.13\033[0m"
+    echo -e "\033[1;36mVersion:\033[0m \033[33m0.14\033[0m"
     echo -e "\033[1;36mTelegram Channel:\033[0m \033[34mhttps://t.me/mirzapanel\033[0m"
     echo -e "\033[1;36mTelegram Group:  \033[0m \033[34mhttps://t.me/mirzapanelgroup\033[0m"
     echo -e "\033[1;36m⭐️Buy Pro Version⭐️: \033[0m \033[34mhttps://t.me/mirzaperimium\033[0m"
